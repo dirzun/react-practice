b2135af39a1237dac94713d895bbf179ebb83ac1
Dirzu Nicu <nicu.dirzu@gmail.com>
2020-02-08 18:05:32 +0200

    practice

---
+++ react-v16up/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import StopWatch from './components/comp';
+import Counter from './components/Click';
+
+function App() {
+  return (
+    <div className="App">
+      <StopWatch></StopWatch>
+      <Counter></Counter>
+    </div>
+  );
+}
+
+export default App;
