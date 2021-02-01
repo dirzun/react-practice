e0107cedf8cdbdd580e7f8b0a49b2e1cd8633190
Dirzu Nicu <nicu.dirzu@gmail.com>
2021-02-01 23:29:15 +0200

    clean git

--- airbnbv2/src/components/Header/Login.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Login =()=>{
-    return (<div>
-        <h1>Log in Baby</h1>
-    </div>);
-}
-export default Login;
--- airbnbv2/src/components/Help/Help.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const Help =()=>{
-    return (<div>Hi baby, you need help</div>);
-}
-export default Help;
